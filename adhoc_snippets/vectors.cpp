e37403d9c08dcb44bc204412cf856f765a1b44eb
Gurkanwal <gurkanwal.singh7@gmaail.com>
2017-05-24 03:16:10 +0530

    vector basic

---
+++ adhoc_snippets/vectors.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    vector<int> v (3,100);
+    vector<int>::iterator it;
+
+    char str[10]="hello";
+    vector<char> u (str, str+ sizeof(str)/sizeof(char));
+    vector<char>::iterator ut;
+
+
+    for (it=v.begin(); it!=v.end(); ++it){
+        cout<<*it<<endl;
+    }
+    for (ut=u.begin(); ut!=u.end(); ++ut){
+        cout<<*ut<<endl;
+    }
+}
